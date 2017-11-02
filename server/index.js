59a235a08e6b1e6f10a1d7c1ea3f1372b58c3439
lichin shao <lichinshao@gmail.com>
2017-11-02 13:00:55 -0700

    set up package.json, dependencies, webpack, and server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,23 @@
-//set up express server here
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, '../client/dist/')));
+
+
+
+
+
+
+
+
+
+app.listen(3100, () => {
+  console.log('listening on port 3100...');
+});
