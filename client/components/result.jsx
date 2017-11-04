e94419974713afe775925800f8216d4541322075
Vitor Pereira <vpereira1982@MacBook-Pro.local>
2017-11-04 16:57:41 -0700

    more changes to result pages -v

--- client/components/result.jsx
+++ client/components/result.jsx
@@ -10,11 +10,15 @@ class Result extends React.Component {
   render() {
     return (
       <div className="jumbotron Col xs={12} md={6}">
-        <h5 >{this.details.restaurant}</h5>
-        <p className="lead">{this.details.item}</p>
-        <p>{this.details.description}</p>
-        <p><h7><strong>Location:</strong> {this.details.location}</h7></p>
-        <p><h7><strong>Price:</strong> ${this.details.price}</h7></p>
+        <h5 className="btn btn-danger" style={{float: 'left', margin: '-25px 0 10px 0'}}>{this.details.restaurant}</h5>
+        <div className="card-block card w-100 text-center" style={{paddingTop: '7px'}}>
+          <p className="lead">{this.details.item}</p>
+          <div className="row justify-content-md-center">
+            <p className="text-justify" style={{maxWidth: '80%'}}>{this.details.description}</p>
+          </div>
+          <p><h7><strong>Location:</strong> {this.details.location}</h7></p>
+          <p><h7><strong>Price:</strong> ${this.details.price}</h7></p>
+        </div>
       </div>
     )
   }
