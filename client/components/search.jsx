a3acd5cc8741be27b2c44d4905da2bf44e0f7ad6
crizzcoxx <crizzcoxx@gmail.com>
2017-11-04 16:46:14 -0700

    centered search

--- client/components/search.jsx
+++ client/components/search.jsx
@@ -2,13 +2,12 @@ const React = require('react');
 
 const Search = () => (
   <form className="form-inline" id="search-boxes">
-    <label className="sr-only" for="inlineFormInput">Name</label>
-    <input type="text" className="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Search Your Food" />
-
-    <label className="sr-only" for="inlineFormInputGroup">Search Your Location</label>
+    <div clss="form-group">
+      <input type="text" className="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Search Your Food" />
+    </div>
     <div className="input-group mb-2 mr-sm-2 mb-sm-0">
-      <div className="input-group-addon">@</div>
-      <input type="text" className="form-control" id="inlineFormInputGroup" placeholder="Username" />
+      <div className="input-group-addon">Location</div>
+      <input type="text" className="form-control" id="inlineFormInputGroup" placeholder="Where are you" />
     </div>
     <button type="submit" className="btn btn-primary">Get Your Hangry On</button>
   </form>
