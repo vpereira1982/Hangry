d85e2ddf138704032ecd42af7b6b30f6133cd2d1
Vitor Pereira <vpereira1982@MacBook-Pro.local>
2017-11-04 17:20:45 -0700

    ajax call progress

--- client/index.jsx
+++ client/index.jsx
@@ -10,17 +10,36 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      example: 'placeholder'
+      list: Sampledata
     };
+  }
+
+  handleSearch(query) {
+    // delete this query;
+    console.log('this is the search query', query)
+    $.ajax({
+      method: "GET",
+      url: "http://localhost:3000" + '/' + query,
+      contentType: 'application/json',
+      success: function(data) {
+        console.log('received data!');
+        this.setState({
+          list: data
+        });
+      },
+      error: function() {
+        console.log('API call failed!')
+      }
+    }).bind(this);
   }
 
   render() {
     return (
       <div>
         <Nav />
-        <Search />
+        <Search handleSearch={this.handleSearch.bind(this)} />
         <div className="container">
-          <ResultsList list={Sampledata} />
+          <ResultsList list={this.state.list} />
         </div>
       </div>
     );
