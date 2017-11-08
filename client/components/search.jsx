69500609a84dc4412c3e2c860dc4897291907968
crizzcoxx <crizzcoxx@gmail.com>
2017-11-07 20:39:19 -0800

    slight tweaks to ui

--- client/components/search.jsx
+++ client/components/search.jsx
@@ -2,33 +2,34 @@ const React = require('react');
 let query = {};
 
 const Search = ({handleSearch}) => (
+
   <form
     onSubmit={(e) => {
       e.preventDefault();
       handleSearch(query);
     }}
-    className="form-inline"
+    className="form-inline container"
     id="search-boxes">
-    {console.log('this is the search form', handleSearch)}
-    <div clss="form-group">
-      <input
-        onChange={(e) => {query.search = e.target.value; }}
-        type="text"
-        className="form-control mb-2 mr-sm-2 mb-sm-0"
-        id="inlineFormInput"
-        placeholder="Search Your Food" />
-    </div>
-    <div className="input-group mb-2 mr-sm-2 mb-sm-0">
-      <div className="input-group-addon">Location</div>
-      <input
-        type="text"
-        onChange={(e) => {query.location = e.target.value; }}
+      {console.log('this is the search form', handleSearch)}
+      <div clss="form-group">
+        <input
+          onChange={(e) => { query.search = e.target.value; }}
+          type="text"
+          className="form-control mb-2 mr-sm-2 mb-sm-0"
+          id="inlineFormInput"
+          placeholder="Search Your Food" />
+      </div>
+      <div className="input-group mb-2 mr-sm-2 mb-sm-0">
+        <div className="input-group-addon">Location</div>
+        <input
+          type="text"
+          onChange={(e) => { query.location = e.target.value; }}
 
-        className="form-control"
-        id="inlineFormInputGroup"
-        placeholder="Where are you" />
-    </div>
-    <button type="submit" className="btn btn-primary">Get Your Hangry On</button>
+          className="form-control"
+          id="inlineFormInputGroup"
+          placeholder="Where are you" />
+      </div>
+      <button type="submit" className="btn btn-primary">Get Your Hangry On</button>
   </form>
 );
 
