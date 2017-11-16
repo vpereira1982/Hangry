a11823eebfff83dabadefa02cb9de780aa6f415b
Matthew Anderson <mattander91@gmail.com>
2017-11-16 15:03:08 -0800

    Added "Description" header for menu item descriptions and made item names h2

--- client/components/result.jsx
+++ client/components/result.jsx
@@ -13,9 +13,9 @@ const Result = (props) => {
     <div className="jumbotron Col xs={12} md={6}">
       <h5 onClick={handleClick} className="btn btn-danger" style={{float: 'left', margin: '-25px 0 10px 0'}}>{details.restaurant}</h5>
       <div className="card-block card w-100 text-center" style={{paddingTop: '7px'}}>
-        <p className="lead">{details.item}</p>
+        <p className="lead"><h2>{details.item}</h2></p>
         <div className="row justify-content-md-center">
-          <p className="text-justify" style={{maxWidth: '80%'}}>{details.description}</p>
+          <p className="text-justify" style={{maxWidth: '80%'}}><strong>Description:</strong> {details.description}</p>
         </div>
         <h6>
           <p><strong>Location:</strong> {details.location}</p>
