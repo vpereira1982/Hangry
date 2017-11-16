e454e2d97f0686c606ba945f884692a2fc9e0561
Matthew Anderson <mattander91@gmail.com>
2017-11-16 14:41:49 -0800

    Added comments for each function in helpers.js

--- server/helpers.js
+++ server/helpers.js
@@ -1,10 +1,12 @@
 const request = require('request');
 
-let getNamesAndKeys = (cityName, foodType, callback) => {
-  var splitFood = foodType.split(' '); // "pepperoni pizza" --> ['pepperoni', 'pizza']
+
+//"Search Restaurants" endpoint: https://developers.eatstreet.com/endpoint/search
+let getNamesAndKeys = (location, foodType, callback) => {
+  var splitFood = foodType.split(' ');
   let query = {
     headers: {'X-Access-Token': '0c8f1aa53d894030'},
-    url: 'https://api.eatstreet.com/publicapi/v1/restaurant/search?method=both&pickup-radius=10&street-address=' + cityName
+    url: 'https://api.eatstreet.com/publicapi/v1/restaurant/search?method=both&pickup-radius=10&street-address=' + location
   };
   request.get(query, (error, response, body) => {
     if (error) {
@@ -16,6 +18,7 @@ let getNamesAndKeys = (cityName, foodType, callback) => {
   });
 };
 
+//Takes body response from getNamesAndKeys and gets restaurant name, location, apiKey (unique restraurant ID), and restaurant address
 let formatRestaurantData = (body, searchedFood) => {
   var res = JSON.parse(body);
   var restaurants = res.restaurants;
@@ -31,7 +34,7 @@ let formatRestaurantData = (body, searchedFood) => {
 };
 
 
-
+//Gets menu items from "Restaurant Menu" endpoint: https://developers.eatstreet.com/endpoint/restaurant-menu
 let getMenu = (apiKey, foodType, callback) => {
   let query = {
     headers: {'X-Access-Token': '0c8f1aa53d894030'},
@@ -43,7 +46,7 @@ let getMenu = (apiKey, foodType, callback) => {
     } else {
       var res = JSON.parse(body);
       var splitFood = cutCommas(foodType.split(' '));
-      if (!res.error) { //see bottom for error message
+      if (!res.error) {
         menusWithRelevance(res, splitFood, function(data) {
           if (data) {
             callback(data);
@@ -56,12 +59,15 @@ let getMenu = (apiKey, foodType, callback) => {
               callback(data);
             }
           });
-        }, 1000);
+        }, 1000); //Can only make 10 requests/second otherwise throws error
       }
     }
   });
 };
 
+//Adds "relevance" property to menu items
+//Only menus items with exact matches are returned
+//ex - search for "Pepperoni Pizza" will only return menu item names or item descriptions that include words "Pepperoni" and "Pizza"
 let menusWithRelevance = (res, splitFood, callback) => {
   var menus = [];
   res.forEach((menu) => {
@@ -87,6 +93,8 @@ let menusWithRelevance = (res, splitFood, callback) => {
   callback(menus);
 };
 
+
+//Extracts menu item name, menu item description (if present, otherwise set to item name), price, and relevance
 let formattedMenu = (apiKey, foodType, callback) => {
   var formattedData = [];
   getMenu(apiKey, foodType, (data) => {
@@ -101,9 +109,11 @@ let formattedMenu = (apiKey, foodType, callback) => {
   });
 };
 
-let menusByCity = (cityName, foodType, callback) => {
+
+//Calls all functions above to retrieve all relevant menu items within 10 miles of provided location/city.
+let menusByCity = (location, foodType, callback) => {
   var menus = [];
-  getNamesAndKeys(cityName, foodType, (restaurants) => {
+  getNamesAndKeys(location, foodType, (restaurants) => {
     if (restaurants) {
       restaurants.forEach( (restaurant) => {
         formattedMenu(restaurant.apiKey, foodType, (menu) => {
@@ -124,6 +134,58 @@ let menusByCity = (cityName, foodType, callback) => {
   }, 2500);
 };
 
+// let menusByCity = (cityName, foodType, callback) => {
+//   var menus = [];
+//   getNamesAndKeys(cityName, foodType, (restaurants) => {
+//     if (restaurants) {
+//       restaurants.forEach((restaurant) => {
+//         formattedMenu(restaurant.apiKey, foodType, (menu) => {
+//           if (menu) {
+//             menu.forEach( (item) => {
+//               var entry = {restaurant: restaurant.name, location: restaurant.location, item: item.name, description: item.description, price: item.price, relevance: item.relevance, address: restaurant.address};
+//               menus.push(entry);
+//             });
+//           }
+//         });
+//       });
+//     }
+//   });
+//   setTimeout(function() {
+//     if (menus) {
+//       var groupByRestaurant = [];
+//       var restaurantNames = [];
+//       menus.forEach(menu => {
+//         if (restaurantNames.includes(menu.restaurant)) {
+//           for (var i = 0; i < groupByRestaurant.length; i++) {
+//             if (groupByRestaurant[i].name === menu.restaurant) {
+//               groupByRestaurant[i].items.push({
+//                 'item' : menu.item,
+//                 'description': menu.description,
+//                 'price': menu.price
+//               });
+//             }
+//           }
+//         } else {
+//           restaurantNames.push(menu.restaurant);
+//           var restaurantObj = {
+//               'name': menu.restaurant,
+//               'items': [],
+//               'address': menu.address,
+//               'location': menu.location
+//             };
+//             restaurantObj.items.push({
+//               'item': menu.item,
+//               'description': menu.description,
+//               'price': menu.price
+//             });
+//             groupByRestaurant.push(restaurantObj);
+//         }
+//       })
+//     };
+//     callback(groupByRestaurant);
+//   }, 1500);
+// };
+
 
 let cutCommas = (array) => {
   return array.map(word => {
@@ -140,13 +202,6 @@ module.exports.getMenu = getMenu;
 module.exports.formattedMenu = formattedMenu;
 module.exports.menusByCity = menusByCity;
 
-
-/*
-response:  { error: true,
-  errorCode: 429,
-  details: 'Per-second rate limit exceeded'
-}
-*/
 
 // let menusByCity = (cityName, foodType, callback) => {
 //   var menus = [];
@@ -218,28 +273,4 @@ response:  { error: true,
 //       callback(formattedData);
 //     }
 //   });
-// };
-
-
-// let menusByCity = (cityName, miles, foodType, callback) => {
-//   var menus = [];
-//   getNamesAndKeys(cityName, miles, foodType, (restaurants) => {
-//     if (restaurants) {
-//       restaurants.forEach( (restaurant) => {
-//         formattedMenu(restaurant.apiKey, foodType, (menu) => {
-//           if (menu) {
-//             menu.forEach( (item) => {
-//               var entry = {restaurant: restaurant.name, location: restaurant.location, item: item.name, description: item.description, price: item.price, relevance: item.relevance};
-//               menus.push(entry);
-//             });
-//           }
-//         });
-//       });
-//     }
-//   });
-//   setTimeout(function() {
-//     if (menus) {
-//       callback(menus);
-//     }
-//   }, 2500);
 // };
