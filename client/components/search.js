2a71f8ac8c3bf538d3daa7d89097422d63896e10
crizzcoxx <crizzcoxx@gmail.com>
2017-11-18 09:49:19 -0800

    changed button

--- client/components/search.js
+++ client/components/search.js
@@ -25,7 +25,7 @@ const Search = ({currFoodSearched, currLocationSearched, handleFoodUserSearch, h
           id="inlineFormInputGroup"
           placeholder="Where are you?" />
       </div>
-      <button type="submit" className="btn btn-primary">Be Full . Be Happy</button>
+      <button type="submit" className="btn btn-primary">Eat Happiness</button>
     </form>
   </div>
 );
