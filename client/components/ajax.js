7086069c28fee734b410cc8d84554160ed0927a1
lichin shao <lichinshao@gmail.com>
2017-11-18 15:28:37 -0800

    fixed syntax error in url for GET and POST request

--- client/components/ajax.js
+++ client/components/ajax.js
@@ -4,7 +4,7 @@ const APIcall = {
   fetch: function(data, endpoint, callback) {
     $.ajax({
       type: 'GET',
-      url: 'https://pure-peak-97033.herokuapp.com/' + endpoint || 'http://127.0.0.1:8080' + endpoint,
+      url: 'https://pure-peak-97033.herokuapp.com' + endpoint || 'http://127.0.0.1:8080' + endpoint,
       data: data,
       success: callback,
       error: function() {
@@ -16,7 +16,7 @@ const APIcall = {
   post: function(data, endpoint, callback) {
     $.ajax({
       type: 'POST',
-      url: 'https://pure-peak-97033.herokuapp.com/' + endpoint || 'http://127.0.0.1:3000' + endpoint,
+      url: 'https://pure-peak-97033.herokuapp.com' + endpoint || 'http://127.0.0.1:3000' + endpoint,
       data: data,
       success: callback,
       error: function() {
