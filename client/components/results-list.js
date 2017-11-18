331f4942e77eefb6c5eabb0ba51642bb37a745ef
crizzcoxx <crizzcoxx@gmail.com>
2017-11-18 12:00:25 -0800

    fix style guide destructuring

--- client/components/results-list.js
+++ client/components/results-list.js
@@ -5,7 +5,7 @@ const Result = require('./result.js');
 const SortList = require('./sort-list.js');
 const UserMessage = require('./user-message.js');
 
-const ResultsList = ({sortList, list, handleClick}) => (
+const ResultsList = ({sortList, list, handleClick, sortHandleClick, showItems, setShowItems}) => (
   list.length > 0
     ? <div className="container">
       <div id="eat-food-title">
@@ -16,6 +16,9 @@ const ResultsList = ({sortList, list, handleClick}) => (
             key={Math.abs(Math.random())}
             result={result}
             sortList={sortList}
+            sortHandleClick={sortHandleClick}
+            showItems={showItems}
+            setShowItems={setShowItems}
           />);
         })};
       </div>
